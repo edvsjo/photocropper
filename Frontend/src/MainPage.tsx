3a311f0c3495de34508e6d0d5921a1c7863c93cf
edvsjo <edvardsjoborg@gmail.com>
2021-07-13 14:45:27 +0200

    Made the folder choice actually matter

--- Frontend/src/MainPage.tsx
+++ Frontend/src/MainPage.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Folder from './folder';
-import {Options, PythonShell} from 'python-shell';
+import { Options, PythonShell } from 'python-shell';
 
 const electron = window.require('electron');
 const remote = electron.remote;
 const app = remote.app;
 
-const rootPath = app.getAppPath().slice(0, app.getAppPath().length - "/Frontend/src".length);
+const rootPath = app
+  .getAppPath()
+  .slice(0, app.getAppPath().length - '/Frontend/src'.length);
 
 function runCropper(path1: string, path2?: string) {
   let options: Options = {
@@ -15,35 +17,25 @@ function runCropper(path1: string, path2?: string) {
     scriptPath: rootPath,
     args: [path1, path2],
   };
-  PythonShell.run(
-    'main.py',
-    options,
-    function (err: any, result: any) {
-      if (err) throw err;
-      console.log('results: %j', result);
-    }
-  );
+  PythonShell.run('main.py', options, function (err: any, result: any) {
+    if (err) throw err;
+    console.log('results: %j', result);
+  });
 }
 
-
-
 const MainPage = () => {
+  const [inputPath, setInputPath] = useState<string>("");
+  const [outputPath, setOutputPath] = useState<string>("")
+
   return (
     <div className="Main-Parrent">
-      <Folder buttonText="Input Folder"></Folder>
-      <Folder buttonText="Output Folder"></Folder>
-      {/* <button onClick={() => runCropper("/Users/sportmannimac/Downloads", "/Users/sportmannimac/Downloads/output")} > {'Run Cropper'}</button> */}
-      <button
-        onClick={() =>
-          runCropper(
-            '/Users/sportmannimac/Downloads/610881_Soft.jpg'
-          )
-        }
-      >
+      <Folder buttonText="Input Folder" callback={setInputPath}></Folder>
+      <Folder buttonText="Output Folder" callback={setOutputPath}></Folder>
+      <button onClick={() => runCropper(inputPath, outputPath)} >
         {'Run Cropper'}
       </button>
     </div>
   );
-}
+};
 
 export default MainPage;
--- Frontend/src/folder.tsx
+++ Frontend/src/folder.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
 const electron = window.require('electron');
-const { shell } = window.require('electron');
 const remote = electron.remote;
 const { dialog } = remote;
 
 type folderProps = {
   buttonText: string;
+  callback: Function;
 };
 
-function openDialog() {
-  dialog
-    .showOpenDialog({
-      title: 'Open Dialogue',
-      message: 'First Dialog',
-      properties: ['openDirectory'],
-    })
-    .then((result: { filePaths: string[] }) => {
-      console.log(result);
-      shell.openPath(result.filePaths[0]);
-      console.log(result.filePaths[0]);
-    });
-}
 
-const Folder = ({ buttonText }: folderProps) => {
+const Folder = ({ buttonText, callback }: folderProps) => {
+  function openDialog() {
+    dialog
+      .showOpenDialog({
+        title: 'Open Dialogue',
+        message: 'First Dialog',
+        properties: ['openDirectory'],
+      })
+      .then((result: { filePaths: string[] }) => {
+        console.log(result.filePaths[0]);
+        callback(result.filePaths[0])
+      });
+  }
   return (
     <>
       <button className="fileBtn" onClick={() => openDialog()}>
